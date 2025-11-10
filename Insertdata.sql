deb80dbe9f335e01c00f5ac9f340f793213e96f2
anithamushimiyimana61-beep <anithamushimiyimana61@gmail.com>
2025-11-10 16:53:09 +0200

    Create Insertdata.sql

---
+++ Insertdata.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee_dropout VALUES
+(1, 'Alice Mwali', 27, 'KIG', 'Accountant', 'Low salary', SYSDATE-10);
+
+INSERT INTO employee_dropout VALUES
+(2, 'Eric mbabazi', 31, 'NOR', 'IT Support', 'Relocated', SYSDATE-20);
+
+COMMIT;
